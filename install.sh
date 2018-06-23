73f2955c01efa44f93375d4f61e195e3a7b40a65
forax <forax@univ-mlv.fr>
2018-06-23 16:04:42 +0200

    fix install shell script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 [ -z "$LATEST_LWORD_COMMIT" ] && export LATEST_LWORD_COMMIT=e5cec7364448
 
 ## download LWORD branch
-[ -f "$LATEST_LWORD_COMMIT.tar.bz2"] && wget http://hg.openjdk.java.net/valhalla/valhalla/archive/$LATEST_LWORD_COMMIT.tar.bz2
+wget http://hg.openjdk.java.net/valhalla/valhalla/archive/$LATEST_LWORD_COMMIT.tar.bz2
 
 ## unpack
 bunzip2 $LATEST_LWORD_COMMIT.tar.bz2
