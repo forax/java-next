958704f8d2a53d5588f281599a97bb40f0e7e5af
forax <forax@univ-mlv.fr>
2018-07-10 14:07:23 +0200

    try to parameterized the build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 [ -z "$JAVA_HOME" ] && export JAVA_HOME=/usr/jdk/jdk-11
 
 ## build the JDK
-cd valhalla
+cd $REPOSITORY
 bash configure --with-boot-jdk=$JAVA_HOME --with-native-debug-symbols=none --disable-warnings-as-errors
 echo "make hotspot"
 LOG=cmdlines make hotspot
@@ -15,7 +15,7 @@ make images
 cd ..
 
 ## copy built jdk
-cp -R valhalla/build/linux-x86_64-normal-server-release/images/jdk/ jdk-11-lworld
+cp -R $REPOSITORY/build/linux-x86_64-normal-server-release/images/jdk/ $ARTIFACT
 
 
 
--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-## env
-[ -z "$LATEST_LWORD_COMMIT" ] && export LATEST_LWORD_COMMIT=ad6949f382f4
-
-## download LWORD branch
-if [ ! -f "$LATEST_LWORD_COMMIT.tar.bz2" ]; then
-   wget http://hg.openjdk.java.net/valhalla/valhalla/archive/$LATEST_LWORD_COMMIT.tar.bz2   
+## download correct branch
+if [ ! -f "$LATEST_COMMIT.tar.bz2" ]; then
+   wget http://hg.openjdk.java.net/$HG_REPOSITORY/$REPOSITORY/archive/$LATEST_COMMIT.tar.bz2   
 fi
 
 ## unpack
-bunzip2 $LATEST_LWORD_COMMIT.tar.bz2
-tar xf $LATEST_LWORD_COMMIT.tar
+bunzip2 $LATEST_COMMIT.tar.bz2
+tar xf $LATEST_COMMIT.tar
 
 ## rename
-mv valhalla-$LATEST_LWORD_COMMIT valhalla
+mv $REPOSITORY-$LATEST_COMMIT $REPOSITORY
 
 
 
