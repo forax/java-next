8509c8ad31114823cc02269ec5a9280a64cf7067
forax <forax@univ-mlv.fr>
2020-07-02 18:37:05 +0200

    change linux dependencies

--- deps-linux.sh
+++ deps-linux.sh
@@ -1,6 +1,6 @@
 sudo apt-get install build-essential
 sudo apt-get install libfreetype6-dev
 sudo apt-get install libcups2-dev
-sudo apt-get install libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libxrandr-dev
+sudo apt-get install ibx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev
 sudo apt-get install libasound2-dev
 sudo apt-get install libffi-dev
